730de80b73d97f25d7c1e8fdd326a537a32a5c14
itork-glitch <piter09102009sx@gmail.com>
2023-08-20 16:49:51 +0200

    🔴Test: 0.1.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,9 +12,9 @@ export const metadata: Metadata = {
   icons: './favicon.ico',
   openGraph: {
     title: {
-      template: '🌊 | WaveCode.pl - Pokaż się w sieci!1',
-      default: '🌊 | WaveCode.pl - Pokaż się w sieci!2',
-      absolute: '🌊 | WaveCode.pl - Pokaż się w sieci!3',
+      template: '🌊 | WaveCode.pl - Pokaż się w sieci!',
+      default: '🌊 | WaveCode.pl - Pokaż się w sieci!',
+      absolute: '🌊 | WaveCode.pl - Pokaż się w sieci!',
     },
     description:
       'Z pomocą WaveCode zaprojektujemy twoje logo, wykonamy twoją wymarzoną nowoczesną witrynę internetową wraz z pełną oprawą wizualną. Stosujemy własne standardy optymalizacji oraz jakości w zakresie tworzenia stron, baz danych i aplikacji internetowych. Pokaż się w sieci!',
@@ -31,6 +32,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <Head>
+        <meta property='og:image' content='https://i.imgur.com/ENaxAEr.png' />
+        <meta name='theme-color' content='#009cff' />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
