736d69c2805d765f3ecad0d13c8d42d9da936518
itork-glitch <piter09102009sx@gmail.com>
2023-08-20 17:01:18 +0200

    🟠Fix: 0.1.6.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,25 +5,12 @@ import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
-/* export const metadata: Metadata = {
+export const metadata: Metadata = {
   title: '🌊 | WaveCode.pl - Pokaż się w sieci!',
   description:
     'Z pomocą WaveCode zaprojektujemy twoje logo, wykonamy twoją wymarzoną nowoczesną witrynę internetową wraz z pełną oprawą wizualną. Stosujemy własne standardy optymalizacji oraz jakości w zakresie tworzenia stron, baz danych i aplikacji internetowych. Pokaż się w sieci!',
   icons: './favicon.ico',
-  openGraph: {
-    title: {
-      template: '🌊 | WaveCode.pl - Pokaż się w sieci!',
-      default: '🌊 | WaveCode.pl - Pokaż się w sieci!',
-      absolute: '🌊 | WaveCode.pl - Pokaż się w sieci!',
-    },
-    description:
-      'Z pomocą WaveCode zaprojektujemy twoje logo, wykonamy twoją wymarzoną nowoczesną witrynę internetową wraz z pełną oprawą wizualną. Stosujemy własne standardy optymalizacji oraz jakości w zakresie tworzenia stron, baz danych i aplikacji internetowych. Pokaż się w sieci!',
-    images: '/icon.png',
-    locale: 'pl-PL',
-    type: 'website',
-    siteName: 'WaveCode.pl',
-  },
-}; */
+};
 
 export default function RootLayout({
   children,
